2bcac782041e5650238951fcfa3fb86760569195
bernie-skipole <bernie@skipole.co.uk>
2021-01-19 14:00:10 +0000

    Adding mqtt communications

--- indidrivers.py
+++ indidrivers.py
@@ -1,11 +1,19 @@
-# Runs indi drivers, storing data to redis
+# stores indi data to redis
 
-from indi_mr import driverstoredis, redis_server
 
+from indi_mr import mqtt_server, redis_server
+
+from indi_mr import mqtttoredis
+# from indi_mr import driverstoredis
+
+# define the hosts/ports where servers are listenning, these functions return named tuples.
+
+mqtt_host = mqtt_server(host='10.105.192.1', port=1883)
 redis_host = redis_server(host='localhost', port=6379)
 
 # blocking call which runs the service, communicating between the drivers and redis
+# driverstoredis(["indi_simulator_telescope", "indi_simulator_ccd"], redis_host, blob_folder='/home/bernard/www/astrodata/served')
 
-driverstoredis(["indi_simulator_telescope", "indi_simulator_ccd"], redis_host, blob_folder='/home/bernard/www/astrodata/served')
-
+# blocking call which runs the service, communicating between mqtt and redis
+mqtttoredis('indi_acremscope', mqtt_host, redis_host, blob_folder='/home/bernard/www/astrodata/served')
 
---
+++ mqttdrivers.py
@@ -0,0 +1,11 @@
+# sends indi data via mqtt
+
+
+from indi_mr import driverstomqtt, mqtt_server
+
+# define the host/port where the MQTT server is listenning, this function returns a named tuple.
+mqtt_host = mqtt_server(host='10.105.192.1', port=1883)
+
+# blocking call which runs the service, communicating between drivers and mqtt
+driverstomqtt(["indi_simulator_telescope", "indi_simulator_ccd"], 'indi_drivers01', mqtt_host)
+
