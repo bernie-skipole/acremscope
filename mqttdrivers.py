ef56924da318e67cacd73aba185c08be892316f0
bernie-skipole <bernie@skipole.co.uk>
2021-01-28 16:46:50 +0000

    Added doordriver.py

--- mqttdrivers.py
+++ mqttdrivers.py
@@ -9,6 +9,7 @@ mqtt_host = mqtt_server(host='10.105.192.1', port=1883)
 # blocking call which runs the service, communicating between drivers and mqtt
 driverstomqtt(["/home/bernard/www/drivers/leddriver.py",
                "/home/bernard/www/drivers/temperaturedriver.py",
+               "/home/bernard/www/drivers/doordriver.py",
                "indi_simulator_telescope",
                "indi_simulator_ccd"], 'indi_drivers01', mqtt_host)
 
