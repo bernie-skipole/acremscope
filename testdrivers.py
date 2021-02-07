2ebd4ad22daa5cb467a241d4cb1d7d1cd89b8d86
bernie-skipole <bernie@skipole.co.uk>
2021-02-07 10:58:39 +0000

    testdrivers for local non mqtt testing

---
+++ testdrivers.py
@@ -0,0 +1,13 @@
+# stores indi data to redis
+
+
+from indi_mr import redis_server, driverstoredis
+
+# define the hosts/ports where servers are listenning, these functions return named tuples.
+
+redis_host = redis_server(host='localhost', port=6379)
+
+# blocking call which runs the service, communicating between the drivers and redis
+driverstoredis(["indi_simulator_telescope", "indi_simulator_ccd"], redis_host, blob_folder='/home/bernard/indiblobs')
+
+
