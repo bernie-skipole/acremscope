e65bf4291860771a74933b9ccb07c39c06849062
bernie-skipole <bernie@skipole.co.uk>
2021-02-20 16:22:17 +0000

    Working on comms to pi

--- mqttdrivers.py
+++ mqttdrivers.py
@@ -7,10 +7,12 @@ from indi_mr import driverstomqtt, mqtt_server
 mqtt_host = mqtt_server(host='10.105.192.1', port=1883)
 
 # blocking call which runs the service, communicating between drivers and mqtt
-driverstomqtt([ "/home/bernard/www/drivers/leddriver.py",
-                "/home/bernard/www/drivers/networkmonitor.py",
-                "/home/bernard/www/drivers/temperaturedriver.py",
-                "/home/bernard/www/drivers/doordriver.py",
-                "indi_simulator_telescope",
-                "indi_simulator_ccd"], 'indi_drivers01', mqtt_host)
+driverstomqtt(["indi_simulator_telescope", "indi_simulator_ccd"], 'indi_drivers01', mqtt_host)
+
+
+# "/home/bernard/www/drivers/leddriver.py",
+# "/home/bernard/www/drivers/networkmonitor.py",
+# "/home/bernard/www/drivers/temperaturedriver.py",
+# "/home/bernard/www/drivers/doordriver.py",
+
 
